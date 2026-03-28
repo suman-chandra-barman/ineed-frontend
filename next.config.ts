9fafc93209666af28b3700277d15874d1ce8ccc1
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2026-03-28 08:31:10 +0600

    feat: add remote pattern for api.hey-ineed.com in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "placehold.co",
       },
+      {
+        protocol: "https",
+        hostname: "api.hey-ineed.com",
+        pathname: "/**",
+      }
     ],
   },
 };
