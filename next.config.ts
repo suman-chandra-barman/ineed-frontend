bf3e6e9a52570666975b1878ff29de33543f8732
suman-chandra-barman <sumanchandrabarman999@gmail.com>
2026-04-04 11:45:51 +0600

    feat: update remotePatterns in Next.js configuration to remove unused entries

--- next.config.ts
+++ next.config.ts
@@ -5,25 +5,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ui-avatars.com",
-        pathname: "/api/**",
-      },
-      {
-        protocol: "https",
-        hostname: "**.devtunnels.ms",
-        pathname: "/media/**",
-      },
-      {
-        protocol: "http",
-        hostname: "**localhost",
-        pathname: "/media/**",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
         hostname: "api.hey-ineed.com",
         pathname: "/**",
       }
